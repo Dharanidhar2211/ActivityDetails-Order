c988e9ae534611053e24b7850034307024fe8f26
Dharanidhar2211 <Dharanidhar220@gmail.com>
2025-07-19 13:46:47 +0530

    PageObject Completea

--- src/test/java/Order/Test/SubmitOrderTest.java
+++ src/test/java/Order/Test/SubmitOrderTest.java
@@ -3,15 +3,10 @@ import PageObjects.CartPage;
 import PageObjects.LandingPage;
 import PageObjects.PaymentPage;
 import PageObjects.ProductCatlogPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import java.time.Duration;
 import java.util.List;
 
